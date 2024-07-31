a699f9e30d35189550af922d39a01a3a31817e82
Ethan Trepka <trepka@stanford.edu>
2024-07-31 16:36:27 -0700

    Transformer completed

---
+++ vaswani2017_attention/README.md
@@ -0,0 +1,21 @@
+# Vaswani et al 2017 Attention is All You Need
+
+### Overview
+This repo contains my PyTorch implementation of the encoder-decoder transformer from the paper, [*Attention is All You Need*](https://www.nature.com/articles/381607a0) by Ashish Vaswani, Noam Shazeer, Niki Parmar, Jakob Uszkoreit, Llion Jones, Aidan N. Gomez, Lukasz Kaiser, and Illia Polosukhin. 
+
+### Results
+In `transformer.py`, I implemented each component of the transformer, including the positional encoder, scaled dot product attention, multiheaded attention, encoder layers, and decoder layers. 
+
+In `test_transformer.ipynb`, I test each component of the transformer that I implemented against the corresponding built in PyTorch functions, and plot the positional encoding as a sanity check. The outputs of my implementation and the PyTorch functions match, suggesting the implementation is correct. 
+
+The plot of the positional encoding is shown below: 
+![Positional encoding](./pos_encoding.png)
+*Positional encoding*
+
+In `translation.ipynb`, I substitute my implementation of the transformer architecture for the built in PyTorch function in a PyTorch transformer tutorial, and demonstrate that it can be succesfully used for translating German words to English after training for a few epochs on the Multi30K dataset.
+
+![Sentence translations](./example_translations.png)
+*Examples translations output by trained model.*
+
+### Acknowledgements
+Referenced the original paper and [NLP course notes](https://web.stanford.edu/class/cs224n/readings/cs224n-self-attention-transformers-2023_draft.pdf). 
--- vaswani2017_attention/transformer.py
+++ vaswani2017_attention/transformer.py
@@ -3,41 +3,41 @@ import torch.nn.functional as F
 from torch import nn
 from collections import OrderedDict
 
+device = 'cpu'
+
 # multihead self-attention implementation
 class MultiHeadedAttention(nn.Module):
-    def __init__(self, d_model, d_k, d_v, h, use_enc_embed=False, use_mask=False, mask=None):
+    def __init__(self, d_model, d_k, d_v, h, use_enc_embed=False):
         super().__init__()
         self.encode = 0
         self.h = h
-        self.W_O = nn.Parameter(torch.rand(h*d_v, d_model))
-        self.attn = nn.ModuleList([ScaledDotProductAttention(d_model, d_k, d_v, use_enc_embed=use_enc_embed, use_mask=use_mask, mask=mask) for _ in range(h)])
+        self.W_O = nn.Parameter(torch.rand(h*d_v, d_model, device=device))
+        self.attn = nn.ModuleList([ScaledDotProductAttention(d_model, d_k, d_v, use_enc_embed=use_enc_embed) for _ in range(h)])
         self.d_v = d_v
 
-    def forward(self, x, enc_embed=None):
-        attn_out = torch.zeros((x.shape[0], x.shape[1], self.d_v*self.h))
+    def forward(self, x, enc_embed=None, use_mask=False, mask=None):
+        attn_out = torch.zeros((x.shape[0], x.shape[1], self.d_v*self.h), device=device)
 
         for i in range(self.h):
-            attn_out[:, :, i*self.d_v:(i+1)*self.d_v] = self.attn[i](x, enc_embed)
+            attn_out[:, :, i*self.d_v:(i+1)*self.d_v] = self.attn[i](x, enc_embed, use_mask=use_mask, mask=mask)
 
         return attn_out @ self.W_O
     
 class ScaledDotProductAttention(nn.Module):
-    def __init__(self, d_model, d_k, d_v, use_enc_embed=False, use_mask=False, mask=None):
+    def __init__(self, d_model, d_k, d_v, use_enc_embed=False):
         super().__init__()
-        self.W_Q = nn.Parameter(torch.rand((d_model, d_k)))
-        self.W_K = nn.Parameter(torch.rand((d_model, d_k)))
-        self.W_V = nn.Parameter(torch.rand(d_model, d_v))
+        self.W_Q = nn.Parameter(torch.rand(d_model, d_k,  device=device))
+        self.W_K = nn.Parameter(torch.rand(d_model, d_k,  device=device))
+        self.W_V = nn.Parameter(torch.rand(d_model, d_v,  device=device))
 
         nn.init.xavier_normal_(self.W_Q)
         nn.init.xavier_normal_(self.W_K)
         nn.init.xavier_normal_(self.W_V)
 
         self.d_k = d_k
-        self.use_mask = use_mask
-        self.mask = mask
         self.use_enc_embed = use_enc_embed
 
-    def forward(self, x, enc_embed):
+    def forward(self, x, enc_embed, use_mask=False, mask=None):
         if self.use_enc_embed: 
             v = torch.matmul(enc_embed, self.W_V)
             k = torch.matmul(enc_embed, self.W_K)
@@ -47,8 +47,7 @@ class ScaledDotProductAttention(nn.Module):
         
         q = torch.matmul(x, self.W_Q)
 
-        if self.use_mask:
-            mask = nn.Transformer.generate_square_subsequent_mask(q.shape[1])
+        if use_mask:
             weights = F.softmax(mask + torch.matmul(q, torch.permute(k, (0, 2, 1)))/torch.sqrt(torch.tensor(self.d_k)), dim=-1)
             return weights @ v
         else:
@@ -83,18 +82,18 @@ class EncoderLayer(nn.Module):
         self.linear2 = nn.Linear(d_ff, d_model)
         self.relu = nn.ReLU()
 
-    def forward(self, x):
-        x = self.layer_norm1(x + self.mha(x))
+    def forward(self, x, use_mask=False, mask=None):
+        x = self.layer_norm1(x + self.mha(x, use_mask=use_mask, mask=mask))
         x = self.layer_norm2(x + self.linear2(self.relu(self.linear1(x))))
         return x
     
 
 # transformer decoder layer implementation
 class DecoderLayer(nn.Module):
-    def __init__(self, d_model, d_k=4, d_v=4, h=4, d_ff = 128, use_mask=False):
+    def __init__(self, d_model, d_k=4, d_v=4, h=4, d_ff = 128):
         super().__init__()
         self.d_model = d_model
-        self.mmha = MultiHeadedAttention(d_model, d_k, d_v, h, use_mask=use_mask, mask=1)
+        self.mmha = MultiHeadedAttention(d_model, d_k, d_v, h)
         self.mha = MultiHeadedAttention(d_model, d_k, d_v, h, use_enc_embed=True)
         self.linear1 = nn.Linear(d_model, d_ff)
         self.linear2 = nn.Linear(d_ff, d_model)
@@ -103,35 +102,76 @@ class DecoderLayer(nn.Module):
         self.layer_norm2 = nn.LayerNorm(d_model)
         self.layer_norm3 = nn.LayerNorm(d_model)
 
-    def forward(self, target, memory):
-        x = self.layer_norm1(target + self.mmha(target))
-        x = self.layer_norm2(x + self.mha(x, memory))
+    def forward(self, target, memory, use_mask=False, mask=None):
+        x = self.layer_norm1(target + self.mmha(target, use_mask=use_mask, mask=mask))
+        x = self.layer_norm2(x + self.mha(x, memory, use_mask=False, mask=None))
         x = self.layer_norm3(x + self.linear2(self.relu(self.linear1(x))))
         return x
     
 # put all the separate components together to make the transformer
 class Transformer(nn.Module):
-    def __init__(self, n_encoder = 6, n_decoder = 6, d_model = 16, n_heads = 2, seq_len = 10):
+    def __init__(self, n_encoder = 6, n_decoder = 6, d_model = 16, n_heads = 2, seq_len = 10, dim_feedforward=2048, use_pos_enc=True, batch_first=True):
         super().__init__()
-        self.encoder = nn.ModuleList([EncoderLayer(d_model, d_v=int(d_model/n_heads), d_k=int(d_model/n_heads), d_ff=2048, n_heads=n_heads) for i in range(n_encoder)])
-        self.decoder = nn.ModuleList([DecoderLayer(d_model, d_v=int(d_model/n_heads), d_k=int(d_model/n_heads), d_ff=2048, h=n_heads) for i in range(n_decoder)])
+        self.encoder = nn.ModuleList([EncoderLayer(d_model, d_v=int(d_model/n_heads), d_k=int(d_model/n_heads), d_ff=dim_feedforward, n_heads=n_heads) for i in range(n_encoder)])
+        self.decoder = nn.ModuleList([DecoderLayer(d_model, d_v=int(d_model/n_heads), d_k=int(d_model/n_heads), d_ff=dim_feedforward, h=n_heads) for i in range(n_decoder)])
         self.pos_encoder = PositionalEncoder(seq_len, d_model)
         self.seq_len = seq_len
         self.n_decoder = n_decoder
         self.n_encoder = n_encoder
         self.layer_norm1 = nn.LayerNorm(d_model)
         self.layer_norm2 = nn.LayerNorm(d_model)
+        self.use_pos_enc = use_pos_enc
+        self.batch_first = batch_first
 
-    def forward(self, input, target):
-        memory = input + self.pos_encoder()
+    def forward(self, input, target, use_enc_mask = False, use_dec_mask = False, enc_mask=1, dec_mask=1):
+        if not self.batch_first:
+            input = torch.permute(input, (1, 0, 2))
+            target = torch.permute(target, (1, 0, 2))
+
+        if self.use_pos_enc:
+            memory = input + self.pos_encoder()
+        else:
+            memory = input
+
         for i in range(self.n_encoder):
-            memory = self.encoder[i](memory)
+            memory = self.encoder[i](memory, use_mask=use_enc_mask, mask=enc_mask)
 
         memory = self.layer_norm1(memory)
         
-        x = target + self.pos_encoder()
+        if self.use_pos_enc:
+            x = target + self.pos_encoder()
+        else:
+            x = target
+
         for i in range(self.n_decoder):
-            x = self.decoder[i](x, memory)
+            x = self.decoder[i](x, memory, use_mask=use_dec_mask, mask=dec_mask)
+        x = self.layer_norm2(x)
+
+        if not self.batch_first:
+            x = torch.permute(x, (1, 0, 2))
+
+        return x
+
+    def encode(self, input, use_enc_mask = False, enc_mask=1):
+        if self.use_pos_enc:
+            memory = input + self.pos_encoder()
+        else:
+            memory = input
+
+        for i in range(self.n_encoder):
+            memory = self.encoder[i](memory, use_mask=use_enc_mask, mask=enc_mask)
+
+        memory = self.layer_norm1(memory)
+        return memory 
+    
+    def decode(self, target, memory, use_dec_mask=True, dec_mask=1):        
+        if self.use_pos_enc:
+            x = target + self.pos_encoder()
+        else:
+            x = target
+
+        for i in range(self.n_decoder):
+            x = self.decoder[i](x, memory, use_mask=use_dec_mask, mask=dec_mask)
         x = self.layer_norm2(x)
 
         return x
